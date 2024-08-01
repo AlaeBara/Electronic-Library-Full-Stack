797b65d4f0f87158b244ae8a9298dbb4feb3fc76
alaeddinebara <baraalaeddine@gmail.com>
2024-08-01 14:41:39 +0100

    Solve Problem Of Category

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import HomePage from './pages/HomePage';
 import MainLayout from './layouts/MainLayout';
 import AllBooks from './pages/AllBooks';
 import NotFound from './pages/NotFound'
-import CategoryBooks from './pages/CategoryBooks'
 import OneBook from './components/OneBook';
 import AddBook from './components/AddBook';
 import SearchResults from './components/SearchResult';
@@ -28,11 +26,9 @@ const AppContent = () => {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path='/' element={<MainLayout />}>
-        <Route index element={<HomePage />} />
-        <Route path='/allbooks' element={<AllBooks />} />
+        <Route index element={<AllBooks />}/>~
         <Route path='/allbooks/:category/:bookId' element={<OneBook />} />
         <Route path='*' element={<NotFound />} />
-        <Route path="/allbooks/:categoryName" element={<CategoryBooks />} />
         <Route path="/search" element={<SearchResults />} />
         <Route path='/login' element={<Sign />} />
         <Route path='/aboutus' element={<AboutUs />} />
