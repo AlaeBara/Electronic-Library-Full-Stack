83d6ca9e4d7b86ddafeaa111ad5aba300f9e7174
Alaeddine Bara <alaeddinebara17@gmail.com>
2024-08-02 10:13:06 +0100

    Update Usermanagement.jsx

--- Front/src/components/admin/Usermanagement.jsx
+++ Front/src/components/admin/Usermanagement.jsx
@@ -6,23 +6,26 @@ const Usermanagement = () => {
   const [users, setUsers] = useState([]);
   const [error, setError] = useState(null);
 
-  useEffect(() => {
-    fetchUsers();
-  }, []);
 
   const fetchUsers = async () => {
     try {
-      const response = await axios.get('http://localhost:5000/api/auth/users', { withCredentials: true });
+      const response = await axios.get('http://localhost:5000/api/admin//users', { withCredentials: true });
       setUsers(response.data);
     } catch (err) {
       setError('Failed to fetch users. Please try again.');
     }
   };
 
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+ 
+
   const handleDelete = async (userId) => {
     try {
       if (window.confirm('Are you sure you want to delete this user?')) {
-        await axios.delete(`http://localhost:5000/api/auth/users/${userId}`, { withCredentials: true });
+        await axios.delete(`http://localhost:5000/api/admin/users/${userId}`, { withCredentials: true });
         fetchUsers();
       }
     } catch (err) {
